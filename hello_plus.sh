7b062056a895af678d602ce53f017e9bd7c03833
alirezatcp <alirezataaty02@gmail.com>
2025-01-06 18:03:34 -0500

    Add another file using shebang.

---
+++ hello_plus.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash  # shebang: write in first of script to run script with that (starts with #!). we can use #!/usr/bin/python3 or ... (after add shebang we dont need to write bash or ... to exec file)
+
+###############################
+# Author: Alireza Taaty
+# Version: v1.0.0
+# Date: 2025-01-06
+# Description: say hello!
+# Usage: ./hello_plus.sh
+##############################
+
+echo "Hello shebang!!"
