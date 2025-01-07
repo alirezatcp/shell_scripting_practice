604c98ad01e8182f4ee23fb266b3d9b34abbbad4
alirezatcp <alirezataaty02@gmail.com>
2025-01-07 08:06:04 -0500

    Add about variables.

--- hello_plus.sh
+++ hello_plus.sh
@@ -5,8 +5,19 @@
 # Author: Alireza Taaty
 # Version: v1.0.0
 # Date: 2025-01-06
-# Description: say hello!
+# Description: our practice script.
 # Usage: ./hello_plus.sh
 ##############################
 
-echo "Hello shebang!!"
+echo "Hello!!"
+
+# write variables like one of this examps: _lowercase  myName  MY_NAME  my_name
+# if we use uppercase to our variables, we should not change them. like AGE=23
+name="Alireza"
+age=22 # linux save all variables as str
+
+echo "my name is $name."
+echo my age is ${age}.
+
+echo $age+2 # result is: 22+2
+echo $(( age+2 )) # result is: 24
